79684ef08c084d9ac1f8e6102337884f6afd12e0
DzabievaAA <DzabievaAA@gmail.com>
2020-10-03 18:11:52 +0300

    Updates

---
+++ precache-manifest.75f6bb5c5d7847356b75d52ee994b9fb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d547ed360495d3923300268234480ec",
+    "url": "/PhotoEditor/index.html"
+  },
+  {
+    "revision": "48186af3de8d0ca2b2e5",
+    "url": "/PhotoEditor/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "a64ff021c204b2182028",
+    "url": "/PhotoEditor/static/css/main.35dca5bd.chunk.css"
+  },
+  {
+    "revision": "48186af3de8d0ca2b2e5",
+    "url": "/PhotoEditor/static/js/2.53b72ea7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/PhotoEditor/static/js/2.53b72ea7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a64ff021c204b2182028",
+    "url": "/PhotoEditor/static/js/main.eee1d943.chunk.js"
+  },
+  {
+    "revision": "bf889d90fa7ed0faed6b",
+    "url": "/PhotoEditor/static/js/runtime-main.e2c43cb1.js"
+  },
+  {
+    "revision": "6022c2d6215c90d97afd341a478905d4",
+    "url": "/PhotoEditor/static/media/kisspng-x-mark-check-mark-clip-art-wrong-sign-5b12e998675839.8614187915279661044233 (1).6022c2d6.png"
+  },
+  {
+    "revision": "d51050a0d11b372c4f04ce1e1a478346",
+    "url": "/PhotoEditor/static/media/logo.d51050a0.svg"
+  }
+]);
